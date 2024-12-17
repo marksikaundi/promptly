e405ecacbdbd26f2813c3c9822a023dee78f9011
marksikaundi <emmanuelmark117@gmail.com>
2024-12-17 02:04:27 +0200

    Remove commented-out Header component from RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ConvexClientProvider } from "./ConvexClientProvider";
-// import {Header} from "@/components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,7 +30,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ConvexClientProvider>
-          {/* <Header /> */}
           {children}</ConvexClientProvider>
 
       </body>
