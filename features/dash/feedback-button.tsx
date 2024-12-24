a9688dd9d1c86a5e112efb267bfbe42a2049489e
marksikaundi <emmanuelmark117@gmail.com>
2024-12-24 04:05:23 +0200

    CORE: Remove Dashboard components and related features

--- features/dash/feedback-button.tsx
+++
@@ -1,9 +0,0 @@
-export function FeedbackButton() {
-    return (
-      <button className="fixed bottom-6 right-6 bg-white border shadow-sm rounded-lg px-4 py-2 text-sm font-medium hover:bg-gray-50">
-        Feedback
-      </button>
-    )
-  }
-  
-  
